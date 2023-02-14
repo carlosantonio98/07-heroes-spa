899bb2f2e579afb205a11278cf45658cfaf68fce
carloscamacho <carlosalvarez9805@gmail.com>
2023-02-13 20:51:50 -0600

    Read arguments by parameter

--- src/heroes/pages/HeroPage.jsx
+++ src/heroes/pages/HeroPage.jsx
@@ -1,8 +1,18 @@
+import { Navigate, useParams } from 'react-router-dom';
+import { getHeroesById } from '../helpers/gerHeroById';
 
 export const HeroPage = () => {
+    const { id } = useParams();  // customHook creado por react-router-dom y nos sirve para acceder a los parametros de nuestra url que se almacenan en el Route.provider
+    const hero = getHeroesById( id );
+
+    if ( !hero ) {
+        return <Navigate to="/marvel" />;  // Navigate es un jsx que podemos regresar en lugar de un <>404 - Not Found</>
+    }
+
     return (
         <>
             <h1>HeroPage</h1>
+            <p>{ hero.alter_ego }</p>
         </>
     );
 }
