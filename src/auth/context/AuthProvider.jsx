ee3b4120f0f305fdd488e400e3fce42c03b3e137
carloscamacho <carlosalvarez9805@gmail.com>
2023-03-23 20:31:35 -0600

    Keep user active with localStorage

--- src/auth/context/AuthProvider.jsx
+++ src/auth/context/AuthProvider.jsx
@@ -4,22 +4,32 @@ import { authReducer } from './AuthReducer';
 
 import { types } from '../types/types';
 
-const initialState = {
+/* const initialState = {  Borramos esto ya que al momento de ejecutar la función de inicialización el estado de nuestro reducer se inicializa automaticamente
     logged: false
+} */
+
+const init = () => {  // inicializamos el estado de nuestro reducer(un reducer es un hook que maneja multiples estado, es como un useState pero con mas control)
+    const user = JSON.parse( localStorage.getItem( 'user' ) );
+
+    return {
+        logged: !!user,
+        user: user
+    }
 }
 
 export const AuthProvider = ({ children }) => {
 
-    const [ authState, dispatch ] = useReducer( authReducer,  initialState );  // destructuramos para sacar el state y la funciión para hacer el dispatch
+    const [ authState, dispatch ] = useReducer( authReducer,  { }, init );  // destructuramos para sacar el state y la funciión para hacer el dispatch
 
     const login = ( name = '' ) => {
+        const user = { id: 'ABC', name }
+
         const action = {
             type: types.login,
-            payload: {
-                id: 'ABC',
-                name: name
-            }
+            payload: user
         }
+
+        localStorage.setItem( 'user', JSON.stringify( user ) ); // No solo podemos actualizar el local storage desde aquí, podemos actualizarlo por ejemplo en un useEffect, el chiste es estar al pendiente de esa información para guardarlo, no se debe de hacer nada del localstorage en el reducer porque ese se encarga solo de manejar estados y disparar funciones
 
         dispatch( action );
     }
