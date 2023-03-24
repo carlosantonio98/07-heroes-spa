ea2243f7484e3963dfcadca4d920bb1255c5c87a
carloscamacho <carlosalvarez9805@gmail.com>
2023-03-24 15:08:53 -0600

    Add private routes

--- src/routes/AppRouter.jsx
+++ src/routes/AppRouter.jsx
@@ -1,8 +1,7 @@
 import { Routes, Route } from 'react-router-dom';
 import { LoginPage } from '../auth';
 import { HeroesRoutes } from '../heroes';
-
-import { Navbar } from '../ui';
+import { PrivateRoute } from './PrivateRoute';
 
 export const AppRouter = () => {
     return (
@@ -11,7 +10,13 @@ export const AppRouter = () => {
 
                 <Route path="login" element={ <LoginPage /> } />
                 
-                <Route path="/*" element={ <HeroesRoutes /> } />
+                <Route path="/*" element={
+                    <PrivateRoute>   {/* creamos un nuevo componente y dentro de este estamos evaluando si el usuario esta logueado o no, si esta logueado muestra los elementos hijos de este higher orden component. */}
+                        <HeroesRoutes />
+                    </PrivateRoute>
+                } />
+                
+                {/* <Route path="/*" element={ <HeroesRoutes /> } /> */}
 
             </Routes>
         </>
