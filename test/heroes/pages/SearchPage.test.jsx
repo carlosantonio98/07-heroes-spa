24b2ca198544bb2bc39bae952dc7fe4eedc99d69
carloscamacho <carlosalvarez9805@gmail.com>
2023-04-06 15:49:03 -0600

    Tests on the SearchPage using jest.requireActual()

--- test/heroes/pages/SearchPage.test.jsx
+++ test/heroes/pages/SearchPage.test.jsx
@@ -1,8 +1,20 @@
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import { MemoryRouter } from 'react-router-dom';
 import { SearchPage } from '../../../src/heroes/pages/SearchPage';
 
+
+const mockedUseNavigate = jest.fn();
+
+// Creamos el mock o clon de una libreria de hook
+jest.mock('react-router-dom', () => ({
+    ...jest.requireActual('react-router-dom'),
+    useNavigate: () => mockedUseNavigate
+}));
+
 describe('Pruebas en <SearchPage />', () => {
+
+    // Limpiamos los metodos de jest.fn() despues de cada test
+    beforeEach(() => jest.clearAllMocks() );
 
     test('Debe de mostrarse correctamente con valores por defecto', () => {
 
@@ -33,6 +45,39 @@ describe('Pruebas en <SearchPage />', () => {
         // si la alerta se error se muestra falla la prueba
         const alert = screen.getByLabelText('alert-danger');
         expect( alert.style.display ).toBe('none');
+
+    });
+
+    test('Debe de mostrar un error si no se encuentra el heroe (batman123)', () => {
+
+        render(
+            <MemoryRouter initialEntries={['/search?q=batman123']}>
+                <SearchPage />
+            </MemoryRouter>
+        );
+
+        const alert = screen.getByLabelText('alert-danger');
+        expect( alert.style.display ).not.toBe('none');
+
+    });
+
+    test('Debe de llamar al navigate a la pantalla nueva', () => {
+
+        const inputValue = 'superman';
+
+        render(
+            <MemoryRouter initialEntries={['/search']}>
+                <SearchPage />
+            </MemoryRouter>
+        );
+
+        const input = screen.getByRole('textbox');
+        fireEvent.change( input, { target: { name: 'searchText', value: inputValue }});
+
+        const form = screen.getByLabelText('form');
+        fireEvent.submit( form );
+
+        expect( mockedUseNavigate ).toHaveBeenCalledWith(`?q=${ inputValue }`);
 
     });
 
